3bd84ec2f6c1b66a8d4dde66feb8f08e58b76f1d
Rly-Aidam <79912173+Rly-Aidam@users.noreply.github.com>
2021-03-08 15:51:15 +0100

    new file

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 let carts = document.querySelectorAll('.pro-add')
 
 var x = 0;
-function myFunction() {
+function idProduktu() {
     var a = document.getElementsByClassName("a");
-    x = a.length;
-    console.log(x)
+    x = a.length;    
 }
 
 function spanNone(){ 
     let b = document.getElementById('span');
-    let vis = localStorage.getItem('cartNumbers');
+    let vis = sessionStorage.getItem('cartNumbers');
     vis = parseInt(vis);
   if(vis != null){
        b.style.display = "";   
-  }
+    }
 }
 
 let products = [     
@@ -77,40 +76,35 @@ for (let i=0; i < carts.length; i++) {
 }
 
 function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
+    let productNumbers = sessionStorage.getItem('cartNumbers');
 
     if(productNumbers) {
         document.querySelector('.kosik span').textContent = productNumbers;
     }
 }
 
-function cartNumbers(product) {
-    console.log("The product clicked is", product);
-    let productNumbers = localStorage.getItem('cartNumbers');
+function cartNumbers(product) {    
+    let productNumbers = sessionStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
 
-
     if( productNumbers ) {
 
-        localStorage.setItem('cartNumbers', productNumbers + 1);
+        sessionStorage.setItem('cartNumbers', productNumbers + 1);
         document.querySelector('.kosik span').textContent = productNumbers + 1;
 
     } else {
 
-        localStorage.setItem('cartNumbers', 1);
+        sessionStorage.setItem('cartNumbers', 1);
         document.querySelector('.kosik span').textContent = 1;
 
     }
-
     setItems(product);   
-
 }
 
 function setItems(product){
-    let cartItems = localStorage.getItem('productsInCart');
-    cartItems = JSON.parse(cartItems);
-    
+    let cartItems = sessionStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);    
     
     if(cartItems != null){
 
@@ -126,42 +120,35 @@ function setItems(product){
          cartItems = {          
             [product.tag]: product
        }   
-    }   
-    
-    localStorage.setItem("productsInCart", JSON.stringify 
+    }       
+    sessionStorage.setItem("productsInCart", JSON.stringify 
     (cartItems));    
 }
 
 function totalCost(product){   
       
-    let cartCost = localStorage.getItem('totalCost');      
+    let cartCost = sessionStorage.getItem('totalCost');      
 
     if(cartCost != null){
         cartCost = parseInt(cartCost);
-        localStorage.setItem("totalCost", cartCost + product.price);
+        sessionStorage.setItem("totalCost", cartCost + product.price);
     } else {
-        localStorage.setItem("totalCost", product.price);         
+        sessionStorage.setItem("totalCost", product.price);         
     }  
 }
 
-
-
 function delItem(){          
-        localStorage.removeItem("productsInCart"); 
-        localStorage.removeItem( "totalCost");
-        localStorage.removeItem("cartNumbers");
-        location.reload();
+    sessionStorage.removeItem("productsInCart"); 
+    sessionStorage.removeItem( "totalCost");
+    sessionStorage.removeItem("cartNumbers");
+    location.reload();
+}
 
-    }
-
-
-function dispayCart() {
-    let cartItems = localStorage.getItem("productsInCart");
+function showCart() {
+    let cartItems = sessionStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems);
     let productContainer = document.querySelector(".container-produktu");
-    let cartCost = localStorage.getItem('totalCost');
-
-    console.log(cartItems);
+    let cartCost = sessionStorage.getItem('totalCost');    
 
     if (cartItems && productContainer ){
         productContainer.innerHTML = '';
@@ -175,8 +162,7 @@ function dispayCart() {
                  <span class="celkem">${item.inCart * item.price},-</span>
               </div>         
               `
-            });   
-            
+            });               
             productContainer.innerHTML +=`
                <div class="kosikCelkove d-flex">
                <img src="pics/x-circle.png" class="del" onclick="delItem();">
@@ -188,10 +174,43 @@ function dispayCart() {
                   </span>
                </div  
             `
+            productContainer.innerHTML +=`
+               <div class="tlacitko-objednat">
+                   <a href="formular.html">
+                   <button type="submit"  class="btn"  >Pokračovat k objednávce</button>     
+                   </a>       
+               </div  
+            `
     }   
 }
 
 
-dispayCart();
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }  
+          form.classList.add('was-validated')
+        }, false)
+      })
+})()
+  
+function Odeslat() {
+    alert("Děkujeme za nákup!");
+}
+
+
+
+showCart();
 spanNone();
 onLoadCartNumbers();
